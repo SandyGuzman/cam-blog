66d8dd66a7c040e91d39832a027397d3675e8091
SandyGuzman <l18121692@morelia.tecnm.mx>
2024-11-14 10:41:46 -0600

    sanityfix

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,15 @@
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemaTypes';
 import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';  
 
 export default defineConfig({
   name: 'default',
   title: 'CAM blog',
 
-  projectId: 'NEXT_PUBLIC_SANITY_PROJECT_ID',
-  dataset: 'NEXT_PUBLIC_SANITY_DATASET',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
-  plugins: [structureTool(), visionTool()],  
+  plugins: [visionTool()],
 
   schema: {
     types: schemaTypes,
